fb4255998bc84ba9250efd2ce12d8bfc934b2133
nicednjsdud <nicednjsdud@naver.com>
2022-04-12 15:52:14 +0900

    fix
    fix right

--- assets/js/plugins/markerwidget.js
+++ assets/js/plugins/markerwidget.js
@@ -1,12 +1,12 @@
 (function(d, h, m){
-    var js, fjs = d.getElementsByTagName(h)[0];
-    if (d.getElementById(m)){return;}
-    js = d.createElement(h); js.id = m;
-    js.onload = function(){
-        window.makerWidgetComInit({
-        position: "left",          
-        widget: "vomkr15afnwdpqby-oq76ymdebrrvwrph-9izzbffxy5eesrvq"                
-    })};
-    js.src = "https://makerwidget.com/js/embed.js";
-    fjs.parentNode.insertBefore(js, fjs)
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "right",          
+            widget: "vomkr15afnwdpqby-oq76ymdebrrvwrph-9izzbffxy5eesrvq"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
 }(document, "script", "dhm"))
